b9e2cfca1842723b4df73e9bb3afd334ade23bae
Emily Bond <emilymbond@yahoo.com>
2014-07-15 11:39:02 -0400

    added basic styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,13 @@
-
-// (function($){
-
 //MODEL
   var Task = Backbone.Model.extend({
     defaults: function(){
       return {
-        todo: '',
-        done: false,
-        _id: ''
+      todo: '',
+      done: false
       };
-    }
-
+    },
+    idAttribute: '_id',
+    urlRoot: 'http://tiny-pizza-server.herokuapp.com/collections/grub'
   });
 
 var task = new Task();
@@ -18,58 +15,40 @@ var task = new Task();
 //COLLECTION
   var TaskList = Backbone.Collection.extend({
     model: Task,
-    url: "http://tiny-pizza-server.herokuapp.com/collections/examples"
+    url: "http://tiny-pizza-server.herokuapp.com/collections/grub"
   });
 
   var taskList = new TaskList();
 
-  // taskList.add([
-  //   {
-  //     todo: 'go to the store'
-  //   }
-  // ]);
-  // taskList.add([
-  //   {
-  //     todo: 'meet up with Dave'
-  //   }
-  // ]);
-  // taskList.add([
-  //   {
-  //     todo: 'pet Bella'
-  //   }
-  // ]);
-
 //VIEW
   var TaskView = Backbone.View.extend({
-    model: new Task,
-    tagName: 'p',
-    // template: $('#todo-list').html(),
+    model: Task,
+    tagName: 'li',
 
-    // initialize: function(){
-    //   this.template = $('#todo-list').html();
-    // },
-    initialize: function(){
-      this.listenTo(this.collection, 'all', this.render);
+    events: {
+      'click .edit' : 'edit',
+      'click .delete' : 'delete',
+      'blur .title' : 'close',
+      'keypress .title' : 'enterSubmit'
+    },
+
+    delete: function(e){
+      e.preventDefault();
+      this.$('.title').attr('editable', true).focus();
+    },
+
+    initialize: function() {
+      this.listenTo(this.collection, 'add', this.render);
+      this.collection.fetch();
     },
 
     render: function(){
-      // this.$el.html(this.template(this.model.toJSON()));
       var source = $('#todo-list').html();
       var template = Handlebars.compile(source);
       var rendered = template({taskList: this.collection.toJSON()})
       this.$el.html(rendered);
       return this;
     }
-    // this.listenTo(this.model, "change", this.render);
-
-    // // If you hit enter its done editing the item.
-    // updateOnEnter: function(e) {
-    //   if (e.keyCode == 13) this.close();
-    // },
-    // // Remove the item, destroy the model.
-    // clear: function() {
-    //   this.model.clear();
-    // }
 
   });
 
@@ -77,34 +56,30 @@ var task = new Task();
     collection: taskList
   })
 
-  var TasksView = Backbone.View.extend({
-    model: taskList,
-    el: $('#todo-container'),
-  });
+  var AppView = Backbone.View.extend( {
 
-// })(jQuery);
+  });
 
 $(document).ready(function(){
   $('#todo-container').append(taskView.render().$el);
   $('#list').submit(function(ev){
       var task = new Task({todo: $('#new-todo').val()});
+      task.save();
       taskList.add(task);
       console.log(taskList.toJSON());
       return false;
     });
 })
 
+// if (e.which !== 13 || !this.input.val().trim()) this.close() ){
+//   return;
+// },
 
+// template: $('#todo-list').html(),
 
-//for handlebars
-// $(function(){
-//   var source = $('#todo-list').html();
-//   var template = Handlebars.compile(source);
-//
-//   // var context = {
-//   //
-//   // };
-//   //
-//   // var html = template(context);
-//   // $(document.body).html(html);
-// });
+// initialize: function(){
+//   this.template = $('#todo-list').html();
+// },
+// initialize: function(){
+//   this.listenTo(this.collection, 'all', this.render);
+// },
